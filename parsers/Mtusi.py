8170ec9f3dea905b420963818266c2ce4670497f
Никита Гришин <nikitagr1205@yandex.ru>
2022-08-01 22:47:22 +0300

    дописал парсер МТУСИ, осталось допарсить места дописал аналит функцию

--- parsers/Mtusi.py
+++ parsers/Mtusi.py
@@ -1,4 +1,5 @@
-from .funcs import get_html, json_save
+from funcs import get_html, json_save,snils_format
+import pyperclip
 
 
 def get_directions():
@@ -27,7 +28,18 @@ def get_students(dir_id):
             'type': 'uch'}
     # FIXME надо ещё получить neuch
     html = get_html("https://lk.abitur.mtuci.ru/ajax.php", data)
-    return {}
+    data['type'] = 'neuch'
+    html2 = get_html("https://lk.abitur.mtuci.ru/ajax.php", data)
+    students = html.find_all('tr', class_='passed') + \
+        html2.find_all('tr')[1:]
+    all_students = {}
+    for i in students:
+        snils = snils_format(i.find_all('td')[2].text) 
+        true_points = 0
+        points = int(i.find_all('td')[8].text or 0)
+        status = i.find_all('td')[10].text != 'Нет'
+        all_students[snils] = {'points':points,'true_points':true_points,'status':status}
+    return all_students
 
 
 def main():
